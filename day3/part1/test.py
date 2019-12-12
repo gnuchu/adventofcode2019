751f6b20e6d1f09987d6395019b0ab966b191966
John Griffiths <gnuchu@gmail.com>
2019-12-12 16:43:59 +0000

    turtling around

--- day3/part1/test.py
+++ day3/part1/test.py
@@ -2,9 +2,39 @@ import sys
 sys.path.append('../../functions')
 import helperfunctions
 import solutionfunctions
+from turtle import *
 
-input = helperfunctions.read_input('input/test.txt')
+def get_heading(direction):
+  heading = 0
+
+  if heading == 'U':
+    heading = 0
+  elif direction == 'D':
+    heading = 180
+  elif direction == 'R': 
+    heading = 90
+  elif direction == 'L':
+    heading = 270
+
+  return heading
+
+input = helperfunctions.read_multiline_input('input/input.txt')
+lines = input.split('\n')
 test_data = input[0].split(',')
+print(lines[1])
 
-result = solutionfunctions.process_direction_instructions(test_data)
+colors = ['red', 'blue']  #Assumption here that we're just drawing 2 lines.
 
+for i, line in enumerate(lines, start=0):
+  instructions = line.split(',')
+  penup()
+  goto(0,0)
+  pendown()
+  pencolor(colors[i])
+
+  for instruction in instructions:
+    direction = instruction[0]
+    steps = int(instruction[1:])/10
+
+    setheading(get_heading(direction))
+    forward(steps)
--- functions/helperfunctions.py
+++ functions/helperfunctions.py
@@ -5,6 +5,12 @@ def read_input(path="input/input.txt"):
   data_array = data.split("\n")
   return data_array
 
+def read_multiline_input(path="input/input.txt"):
+  with open(path, "r") as stream:
+    data = stream.read()
+  
+  return data
+
 def write_output(data, path="output/solution.txt"):
   with open(path, "w") as file:
     file.write(data)
